844c65e1f270b93570b2f9dfc3a844b0c2185ca7
rash0x6964 <rash0x6964@gmail.com>
2024-04-01 14:30:08 +0000

    styling links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import classnames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
 
 export const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "issue", href: "/issues" },
@@ -17,7 +23,11 @@ export const NavBar = () => {
         {links.map((item) => (
           <Link
             key={item.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"	
+            className={classnames({
+              "text-zinc-900": item.href === currentPath,
+              "text-zinc-500": item.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             href={item.href}
           >
             {item.label}
