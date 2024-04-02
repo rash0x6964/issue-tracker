489e95a07e31f6a4101660f57dff655b421af412
rash0x6964 <rash0x6964@gmail.com>
2024-04-02 20:06:18 +0000

    add additional loading skeletons

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -12,10 +12,11 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+import delay from "delay";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
-const NewIssuePage = () => {
+const NewIssuePage = async () => {
   const {
     register,
     control,
@@ -38,6 +39,8 @@ const NewIssuePage = () => {
       setError("An unexpected error occurred.");
     }
   });
+  await delay(2000);
+
 
   return (
     <div className="max-w-xl">
