664c0762afb04ba1694e4353afcaaf7b4e3551e3
rash0x6964 <rash0x6964@gmail.com>
2024-04-03 14:31:29 +0000

    build an API for updating issues

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,6 +1,6 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 
 export async function GET(request: NextRequest) {
   const issues = await prisma.issue.findMany();
@@ -9,7 +9,7 @@ export async function GET(request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const validation = createIssueSchema.safeParse(body);
+  const validation = issueSchema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.format(), { status: 400 });
 
