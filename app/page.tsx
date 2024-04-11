24e687beb22e8bc9d72c7f1ed7a88e4f4affe154
rash0x6964 <rash0x6964@gmail.com>
2024-04-11 13:30:18 +0000

    build the IssueSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,28 @@ import { Flex, Text, Button } from "@radix-ui/themes";
 import Image from "next/image";
 import Pagination from "./components/Pagination";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
-  return <LatestIssues />;
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+
+  return (
+    <>
+      <LatestIssues />
+      <IssueSummary open={open} closed={closed} inProgress={inProgress} />
+    </>
+  );
 }
