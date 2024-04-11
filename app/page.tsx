f692e8b8eec2c92c98bf0a6a723b11aceff7ef39
rash0x6964 <rash0x6964@gmail.com>
2024-04-11 12:13:41 +0000

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,20 @@ import { Flex, Text, Button } from "@radix-ui/themes";
 import Image from "next/image";
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <Flex direction="column" gap="2">
       <Text>Hello from Radix Themes :)</Text>
       <Button>Let's go</Button>
-      <Pagination itemCount={100} currentPage={1} pageSize={10} />
+      <Pagination
+        itemCount={100}
+        currentPage={parseInt(searchParams.page)}
+        pageSize={10}
+      />
     </Flex>
   );
 }
