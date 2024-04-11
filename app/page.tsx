467ee6da158fa2da61d70738deb65a7e998ad78c
rash0x6964 <rash0x6964@gmail.com>
2024-04-11 11:58:33 +0000

    build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Flex, Text, Button } from "@radix-ui/themes";
 import Image from "next/image";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
   return (
     <Flex direction="column" gap="2">
       <Text>Hello from Radix Themes :)</Text>
       <Button>Let's go</Button>
+      <Pagination itemCount={100} currentPage={1} pageSize={10} />
     </Flex>
   );
 }
