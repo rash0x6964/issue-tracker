304007aad6ea2fc205723ba97d3f7e276dd45e1a
rash0x6964 <rash0x6964@gmail.com>
2024-04-11 13:27:33 +0000

    build the LatestIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,12 @@
 import { Flex, Text, Button } from "@radix-ui/themes";
 import Image from "next/image";
 import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
 export default function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
-  return (
-    <Flex direction="column" gap="2">
-      <Text>Hello from Radix Themes :)</Text>
-      <Button>Let's go</Button>
-      <Pagination
-        itemCount={100}
-        currentPage={parseInt(searchParams.page)}
-        pageSize={10}
-      />
-    </Flex>
-  );
+  return <LatestIssues />;
 }
